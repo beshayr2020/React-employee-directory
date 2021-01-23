408807a430dcf44f8103f5c1c457765ea26c93c0
Beshayr Said <68831944+beshayr2020@users.noreply.github.com>
2021-01-23 16:32:26 -0600

    update the header text

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,8 +4,7 @@ import "./Header.css";
 function Header () {
     return (
         <div className="header">
-            <h1>Employee Directory</h1>
-            <p>Click on each heading to filter or use the search box to narrow your results</p>
+            <h1>Employee Directory</h1>            
         </div>
     )
 }
